d890c0e221d2279c799f43f5c542fa8daddaad1f
Arthur van de Vondervoort <arthur@MBP-van-Arthur.lan>
2019-05-25 19:02:07 +0200

    Add map to ReligionComp

--- src/app/religion/religion.module.ts
+++ src/app/religion/religion.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { ReligionRoutingModule } from './religion-routing.module';
 import { ShareModule } from '../share/share.module';
 import { ReligionComponent } from './religion.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
-  declarations: [ReligionComponent],
+  declarations: [ReligionComponent, MapComponent],
   imports: [
     CommonModule,
     ReligionRoutingModule,
