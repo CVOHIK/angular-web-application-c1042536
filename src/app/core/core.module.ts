6cc101760468c76dfafd1087baf67df3fb6feeb5
Arthur van de Vondervoort <arthur@MBP-van-Arthur.lan>
2019-05-25 18:59:20 +0200

    Include material en http to core

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,14 +4,20 @@ import { CommonModule } from '@angular/common';
 import { CoreRoutingModule } from './core-routing.module';
 import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
-import { BrowserAnimationsModule}  from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatMenuModule, MatButtonModule, MatIconModule } from '@angular/material'
+import { HttpClientModule } from '@angular/common/http'; 
 
 @NgModule({
   declarations: [HeaderComponent],
   imports: [
     CommonModule,
     CoreRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    HttpClientModule
   ],
   exports: [
     RouterModule,
